a49198bfa5c55e4644438804d05c27b5ae753637
RosyG <rmga2106@gmail.com>
2018-07-08 22:17:46 -0500

    fix(todo):add input in todo

--- src/app/bitcoin-prices/bitcoin-prices.component.ts
+++ src/app/bitcoin-prices/bitcoin-prices.component.ts
@@ -3,14 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-bitcoin-prices',
   templateUrl: './bitcoin-prices.component.html',
-  styleUrls: ['./bitcoin-prices.component.css']
+  styleUrls: ['./bitcoin-prices.component.css'],
+  template: `
+    <label>Fecha de inicio</label>
+    <input type="datetime-local" [(ngModel)]="start_date">
+    <label>Fecha final</label>
+    <input type="datetime-local" [(ngModel)]="end_date">
+    <button (click)="setDate()">Set value</button>
+  `,
+
 })
 export class BitcoinPricesComponent implements OnInit {
-  public newDate;
-
-  constructor() {
-    this.newDate = new Date(2017, 4, 12);
+  public start_date;
+  public end_date;
+  public part1;
+  setDate() {
+    console.log(this.start_date, 'inicio', this.end_date, 'fin');
   }
+  constructor(
+  ) { }
 
   ngOnInit() {
   }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
-
+  fecha = '';
+  task = '';
+  newTask = {};
+  todoList = [];
+  addTask() {
+    this.newTask = { 'task': this.task, 'date': this.fecha};
+    this.todoList.push(this.newTask);
+    console.log(this.todoList);
+   }
   constructor() { }
 
   ngOnInit() {
   }
-
 }
