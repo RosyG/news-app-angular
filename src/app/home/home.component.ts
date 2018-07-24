650b3f67df424c51c9eaae14e3f80b3186f3230c
rmga <rmga.work@gmail.com>
2018-07-24 17:13:47 -0500

    fix(home):Copy text

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  public texts = ['texto1', 'texto2', 'texto3', 'texto4'];
+  in: string;
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  fromText(select) {
+    console.log(select);
+  }
+  toText(): void {
+    this.in = 'zas';
+    console.log(this.in, 'el texto');
   }
 
 }
